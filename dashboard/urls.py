b83e6b4d66a154975b051860a0d226d28b8a772e
Boyuan Liu <boyuanliu6@yahoo.com>
2023-07-12 16:48:12 -0700

    added standards charts and edit question view'

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("", views.dashboard),
     path("course/<int:course_id>/", views.course),
     path("course/<int:course_id>/add-question", views.add_question_view),
-    path("course/<int:course_id>/view-questions/s/<int:standard_id>/", views.view_questions_by_standard)
+    path("course/<int:course_id>/view-questions/s/<int:standard_id>/", views.view_questions_by_standard),
+    path("course/<int:course_id>/edit-question/<int:question_id>", views.edit_question),
+    path("api/course/<int:course_id>/standards-stats", views.api_get_standards_statistics)
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, render
+from django.http import JsonResponse
 from .models import MCOption, MCQuestion, Course, Standard
 from django.contrib import messages
 from helpers import upload_image
@@ -51,14 +52,12 @@ def add_question_view(request, course_id):
     if request.method == "POST":
 
         if len(request.FILES) != 0:
-            print(request.FILES["image"])
             url = upload_image(base64.b64encode(request.FILES["image"].file.read()).decode('utf-8'))
             question = MCQuestion.objects.create(course=course, question=request.POST["question"], image_url=url)
         else:
             question = MCQuestion.objects.create(course=course, question=request.POST["question"])
 
         for short_name in request.POST.getlist("standard"):
-            print(short_name, request.POST["standard"], request.POST)
             question.standards.add(Standard.objects.get(course=course, short_name=short_name))
             question.save()
 
@@ -93,4 +92,73 @@ def view_questions_by_standard(request, course_id, standard_id):
         "questions": questions,
         "course": course,
         "standard": standard
+    })
+
+def edit_question(request, course_id, question_id):
+    course = Course.objects.get(id=course_id)
+    standards = Standard.objects.filter(course=course)
+    question = MCQuestion.objects.get(course=course, id=question_id)
+    
+    if request.method == "POST":
+
+        question.question = request.POST["question"]
+        if len(request.FILES) != 0:
+            url = upload_image(base64.b64encode(request.FILES["image"].file.read()).decode('utf-8'))
+            question.image_url = url
+        
+        question.standards.clear()
+
+        for short_name in request.POST.getlist("standard"):
+            question.standards.add(Standard.objects.get(course=course, short_name=short_name))
+
+        question.choices.clear()
+
+        for opt in ['a', 'b', 'c', 'd', 'e']:
+            if opt in request.POST.getlist("correct"):
+                opt = MCOption.objects.create(text=request.POST[f"opt-{opt}-val"], is_correct=True)
+            else:
+                opt = MCOption.objects.create(text=request.POST[f"opt-{opt}-val"], is_correct=False)
+            
+            question.choices.add(opt)
+        
+        question.save()
+
+        messages.add_message(request, messages.SUCCESS, "Question edited successfully")
+        return redirect(f"/course/{course.id}/edit-question/{question.id}")
+    else:
+
+        return render(request, "dashboard/edit-question.html", {
+            "standards": standards,
+            "question": question,
+            "choices": question.choices.all(),
+            "checked_standards": question.standards.all()
+        })
+
+def api_get_standards_statistics(request, course_id):
+    course = Course.objects.get(id=course_id)
+    standards = Standard.objects.filter(course=course)
+    questions = MCQuestion.objects.filter(course=course)
+    
+    standards_question_counter = {}
+    
+    for standard in standards:
+        standards_question_counter[standard.short_name] = 0
+    
+    for question in questions:
+        for standard in question.standards.all():
+            standards_question_counter[standard.short_name] += 1
+    
+    return JsonResponse({
+        "labels": list(standards_question_counter.keys()),
+        "datasets": [{
+            "label": "Question(s)",
+            "data": list(standards_question_counter.values()),
+            "backgroundColor": [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)',
+                'rgb(54, 76, 255)',
+            ],
+            "hoverOffset": 4
+        }]
     })
